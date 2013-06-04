4c87d8c8c2a13f5e27a2a53aea587da8b3b9768a
Kenny Cason <kenneth.cason@gmail.com>
2013-06-04 12:59:29 -0500

    minor stuff

--- Layer.hs
+++ Layer.hs
@@ -32,15 +32,11 @@ sumNodeError node childLayer = foldl (+) 0 (listProduct (weights node) (errors c
 calculateNodeError :: Node -> Layer -> Double
 calculateNodeError node childLayer = (sumNodeError node childLayer) * (value node) * (1.0 - (value node))
 
-{-
+
 calculateErrors :: Layer -> Layer -> Layer
 calculateErrors layer childLayer = Layer (nodes layer)
                                          (map calculateNodeError (nodes layer) childLayer ) -- need to map each of the nodes in the layer, and the childLayer to calculateNodeError
                                          (teacherSignals layer)
                                          (learningRate layer)
--}
 
--- (map even [1..10])
-
--- calculateErrors :: Layer -> Layer
--- calculateErrors =              
+         
